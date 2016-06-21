7478e0a270e48ff8f3f24adf8a05425a44cf54a6
AngelaRg <angela_9412@hotmail.com>
2016-06-20 19:08:41 -0500

    Inicio

---
+++ EnMTY/app/src/main/java/mx/itesm/enmty/TauMainActivity.java
@@ -0,0 +1,13 @@
+package mx.itesm.enmty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TauMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tau_main);
+    }
+}
